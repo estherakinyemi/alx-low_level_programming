d3b54dd6659352594f4d77c354f46233acbb18a6
Esther Akinyemi <akinyemifolasikemi@gmail.com>
2022-10-14 16:08:10 -0700

    Still Putchar

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ ** main - Entry point
+ **
+ ** Return: Always 0 (Success)
+ **/
+
+int main(void)
+{
+	char d;
+
+	for (d = 'z' ; d >= 'a' ; d--)
+	{
+		putchar(d);
+	}
+	putchar('\n');
+	return (0);
+}
