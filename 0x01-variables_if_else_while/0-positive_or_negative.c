04c5dcc9f895ac279543f9bd372555b56505135d
Esther Akinyemi <akinyemifolasikemi@gmail.com>
2022-10-14 13:50:41 -0700

    Random number variables and if

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* header libraries necessary for code to be executeable*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d", n);
+		printf(" is positive\n");
+	}
+	if (n < 0)
+	{
+		printf("%d",n );
+		printf(" is negative\n");
+	}
+	return (0);
+}
