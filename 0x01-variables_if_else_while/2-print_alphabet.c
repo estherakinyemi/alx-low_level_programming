857959350015c741fd455d275d8adac597020aa8
Esther Akinyemi <akinyemifolasikemi@gmail.com>
2022-10-14 15:15:06 -0700

    Putchar in C

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ ** main -Entry point
+ **
+ ** Return: Always 0 (Success)
+ **/
+
+int main(void)
+{
+	char ad;
+	for (ad = 'a' ; ad <= 'z' ; ad++)
+	{
+	putchar(ad);
+	}
+	putchar('\n');
+	return (0);
+}
