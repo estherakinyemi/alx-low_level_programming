a7114d4c42acaf373dbcf997fc11651e6aeb95ac
Esther Akinyemi <akinyemifolasikemi@gmail.com>
2022-10-14 17:14:56 -0700

    Only Putchar

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ ** main - Entry point
+ **
+ ** Return: Always 0 (Success)
+ **/
+
+int main(void)
+{
+	char f;
+
+	for (f = '0' ; f <= '9' ; f++)
+	{
+		putchar(f);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
