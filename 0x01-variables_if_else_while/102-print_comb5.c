df2ad8b31c42085a74882bb6051b4cc9a805dd82
Esther Akinyemi <akinyemifolasikemi@gmail.com>
2022-10-14 18:47:58 -0700

    Last commit in Putchar

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ ** main - Entry point
+ **
+ ** Return: Always 0 (Success)
+ **/
+int main(void)
+{
+	int y, z;
+
+	for (y = 0 ; y <= 98 ; y++)
+	{
+		for (z = y + 1 ;  z <= 99 ; z++)
+		{
+			putchar((y / 10) + '0');
+			putchar((y % 10) + '0');
+			putchar(' ');
+			putchar((z / 10) + '0');
+			putchar((z % 10) + '0');
+			if (y == 98 && z == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
