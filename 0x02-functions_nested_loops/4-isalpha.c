e154de762b81c2ce0e365320f80efc629f41c353
Esther Akinyemi <akinyemifolasikemi@gmail.com>
2022-10-18 17:04:47 -0700

    i

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ ** _isalpha - Discovers characters that are alphabets
+ **
+ ** @p: single letter input
+ **
+ ** Return: 1 for all letters, 0 for others
+ **/
+
+int _isalpha(int p)
+{
+	if (((p >= 'a') && (p >= 'z')) || ((p >= 'A') && (p <= 'Z')))
+	return (0);
+	else
+	return (0);
+}
