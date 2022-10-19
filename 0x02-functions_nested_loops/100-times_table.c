45fefed89a493b76593d15da04940525c75cd817
Esther Akinyemi <akinyemifolasikemi@gmail.com>
2022-10-19 10:33:58 -0700

    sum of multiples of 5 and 3

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ ** print_times_table -  Prints the times table of the input
+ **
+ **@n: Input
+ **
+ ** Return: Always 0 (Success)
+ **/
+
+void print_times_table(int n)
+{
+
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ ** main - Entry point
+ **
+ ** Return: Always 0 (Success)
+ **/
+
+int main(void)
+{
+	int i, sum;
+	sum = 0;
+	for(i = 0 ; i < 1024 ; i++)
+	{
+		if ((i % 5 == 0) || (i % 3 == 0))
+		{
+			sum = sum + i;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
