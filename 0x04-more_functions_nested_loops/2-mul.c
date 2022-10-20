1bc5d92da397ce31297787b5dceee288a25e73c3
Esther Akinyemi <akinyemifolasikemi@gmail.com>
2022-10-20 15:15:19 -0700

    MUL

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Multiples two inputed integerd
+ * @a: First number inputed
+ * @b: Second number inputed
+ * Return: Results of the multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
