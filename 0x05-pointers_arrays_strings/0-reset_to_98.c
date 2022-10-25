0c0ccbb264a864be7857e294634a3ed580492b43
Esther Akinyemi <akinyemifolasikemi@gmail.com>
2022-10-25 09:48:00 -0700

    0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - Prints a variable to 98
+ * @n: The number to be printed
+ * Return: 98
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Points, Arrays and Strings in C
