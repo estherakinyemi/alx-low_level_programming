86ca649e1cae82faa78d7ebcb34babdbfd3d2c16
Esther Akinyemi <akinyemifolasikemi@gmail.com>
2022-10-27 16:39:08 -0700

    More on Strings, Arrays and pointers

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - Reverses the content of an array of integers.
+ * @a: The array of integers to be reversed.
+ * @n: The number of elements in array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int tmp, beg = 0;
+	int end = n - 1;
+
+	while (beg < end)
+	{
+		tmp = *(a + beg);
+		*(a + beg) = *(a + end);
+		*(a + end) = tmp;
+		beg++, end--;
+	}
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+More on Strings, Arrays and pointers
